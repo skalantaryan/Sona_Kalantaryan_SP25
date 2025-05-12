59c7745cb7c7bd14a26c45411bec1e77600ad43d
skalantaryan <sqalantaryan@gmail.com>
2025-05-13 03:09:45 +0400

    Uploaded the updated homework files

--- SQL_DDL_Func_Sona_Kalantaryan_HW.sql
+++ SQL_DDL_Func_Sona_Kalantaryan_HW.sql
@@ -1,14 +1,16 @@
+BEGIN;
+
 --1
 CREATE OR REPLACE VIEW sales_revenue_by_category_qtr AS
 SELECT 
 	c.name AS category,
 	SUM(p.amount) AS total_revenue
 FROM category c
-JOIN film_category fc ON c.category_id = fc.category_id
-JOIN film f ON fc.film_id = f.film_id
-JOIN inventory i ON f.film_id = i.film_id
-JOIN rental r ON i.inventory_id = r.inventory_id
-JOIN payment p ON r.rental_id = p.rental_id
+INNER JOIN film_category fc ON c.category_id = fc.category_id
+INNER JOIN film f ON fc.film_id = f.film_id
+INNER JOIN inventory i ON f.film_id = i.film_id
+INNER JOIN rental r ON i.inventory_id = r.inventory_id
+INNER JOIN payment p ON r.rental_id = p.rental_id
 WHERE DATE_PART('year', p.payment_date) = DATE_PART('year', CURRENT_DATE) AND --take only the year of p.payment_date and current date
 	DATE_PART('quarter', p.payment_date) = DATE_PART('quarter', CURRENT_DATE) --take the quarter based on p.payment_date and current date
 GROUP BY c.category_id
@@ -16,28 +18,41 @@ HAVING SUM(p.amount) > 0;
 
 
 --2
-CREATE OR REPLACE FUNCTION get_sales_revenue_by_category_qtr (quarter INT, year INT)
-	RETURNS TABLE(
-		category TEXT, 
-		total_revenue NUMERIC) 
-	AS $$
+CREATE OR REPLACE FUNCTION get_sales_revenue_by_category_qtr (
+    quarter_year TEXT  -- for instance '2024-Q1'
+)
+RETURNS TABLE (
+    category TEXT, 
+    total_revenue NUMERIC
+)
+AS $$
+DECLARE
+    qtr INT;
+    yr INT;
 BEGIN
-	RETURN QUERY
-	SELECT 
-		c.name AS category,
-		SUM(p.amount) AS total_revenue
-	FROM category c
-	JOIN film_category fc ON c.category_id = fc.category_id
-	JOIN film f ON fc.film_id = f.film_id
-	JOIN inventory i ON f.film_id = i.film_id
-	JOIN rental r ON i.inventory_id = r.inventory_id
-	JOIN payment p ON r.rental_id = p.rental_id
-	WHERE DATE_PART('year', p.payment_date) = year AND
-		DATE_PART('quarter', p.payment_date) = quarter
-	GROUP BY c.category_id
-	HAVING SUM(p.amount) > 0;
+	--Check if the input was accurate
+	IF quarter_year NOT LIKE '____-Q_' THEN
+        RAISE EXCEPTION 'Invalid format: use YYYY-Qn (e.g., 2024-Q1)';
+    END IF;
+    yr := SPLIT_PART(quarter_year, '-', 1)::INT; --take the YEAR FROM the text parameter
+    qtr := RIGHT(quarter_year, 1)::INT; --take the quearter FROM the parameter
+    RETURN QUERY
+    SELECT 
+        c.name AS category,
+        SUM(p.amount) AS total_revenue
+    FROM category c
+    INNER JOIN film_category fc ON c.category_id = fc.category_id
+    INNER JOIN film f ON fc.film_id = f.film_id
+    INNER JOIN inventory i ON f.film_id = i.film_id
+    INNER JOIN rental r ON i.inventory_id = r.inventory_id
+    INNER JOIN payment p ON r.rental_id = p.rental_id
+    WHERE DATE_PART('year', p.payment_date) = yr
+      AND DATE_PART('quarter', p.payment_date) = qtr
+    GROUP BY c.category_id
+    HAVING SUM(p.amount) > 0;
 END;
 $$ LANGUAGE plpgsql;
+
 
 --3
 CREATE OR REPLACE FUNCTION get_most_popular_film_by_country(country_names TEXT[])
@@ -59,31 +74,32 @@ BEGIN
 		f.length AS length,
 		f.release_year::integer AS release_year
 	FROM film f
-	JOIN language l ON f.language_id = l.language_id
-	JOIN inventory i ON f.film_id = i.film_id
-	JOIN rental r ON i.inventory_id = r.inventory_id
-	JOIN customer cm ON r.customer_id = cm.customer_id
-	JOIN address a ON cm.address_id = a.address_id
-	JOIN city ct ON a.city_id = ct.city_id
-	JOIN country c ON ct.country_id = c.country_id
+	INNER JOIN language l ON f.language_id = l.language_id
+	INNER JOIN inventory i ON f.film_id = i.film_id
+	INNER JOIN rental r ON i.inventory_id = r.inventory_id
+	INNER JOIN customer cm ON r.customer_id = cm.customer_id
+	INNER JOIN address a ON cm.address_id = a.address_id
+	INNER JOIN city ct ON a.city_id = ct.city_id
+	INNER JOIN country c ON ct.country_id = c.country_id
 	WHERE c.country = ANY(country_names)
 	GROUP BY c.country, f.title, f.rating, l.name, f.length, f.release_year
 	HAVING COUNT(r.rental_id) = (
 		SELECT MAX(cnt) FROM (
             SELECT COUNT(r2.rental_id) AS cnt
             FROM rental r2
-            JOIN inventory i2 ON r2.inventory_id = i2.inventory_id
-            JOIN film f2 ON i2.film_id = f2.film_id
-            JOIN customer cm2 ON r2.customer_id = cm2.customer_id
-            JOIN address a2 ON cm2.address_id = a2.address_id
-            JOIN city ct2 ON a2.city_id = ct2.city_id
-            JOIN country c2 ON ct2.country_id = c2.country_id
+            INNER JOIN inventory i2 ON r2.inventory_id = i2.inventory_id
+            INNER JOIN film f2 ON i2.film_id = f2.film_id
+            INNER JOIN customer cm2 ON r2.customer_id = cm2.customer_id
+            INNER JOIN address a2 ON cm2.address_id = a2.address_id
+            INNER JOIN city ct2 ON a2.city_id = ct2.city_id
+            INNER JOIN country c2 ON ct2.country_id = c2.country_id
             WHERE c2.country = c.country
             GROUP BY f2.title
         ) AS subquery
 	);
 END;
 $$ LANGUAGE plpgsql;
+
 
 --4
 CREATE OR REPLACE FUNCTION films_in_stock_by_title(title_type TEXT)
@@ -103,11 +119,18 @@ BEGIN
 		c.first_name || c.last_name AS customer_name,
 		r.rental_date
 	FROM film f
-	JOIN language l ON f.language_id = l.language_id
-	JOIN inventory i ON f.film_id = i.film_id
-	JOIN rental r ON i.inventory_id = r.inventory_id
-	JOIN customer c ON r.customer_id = c.customer_id
-	WHERE LOWER(f.title) LIKE title_type 
+	INNER JOIN language l ON f.language_id = l.language_id
+	INNER JOIN inventory i ON f.film_id = i.film_id
+	INNER JOIN rental r ON i.inventory_id = r.inventory_id
+	INNER JOIN customer c ON r.customer_id = c.customer_id
+	WHERE LOWER(f.title) LIKE LOWER(title_type) 
+		AND r.return_date IS NOT NULL
+		AND r.rental_date = (
+			SELECT MAX(r2.rental_date)
+			FROM rental r2
+			WHERE r2.inventory_id = r.inventory_id
+				AND r2.return_date IS NOT NULL
+		)
 	GROUP BY f.title, l.name, c.first_name || c.last_name, r.rental_date
 	HAVING COUNT(i.inventory_id) > 0
 	ORDER BY row_num;
@@ -125,9 +148,9 @@ CREATE OR REPLACE PROCEDURE new_movie(
 LANGUAGE plpgsql 
 AS $$
 DECLARE 
-	language_id INT;
+	lang_id INT;
 BEGIN
-	SELECT language_id INTO language_id
+	SELECT language_id INTO lang_id
 	FROM language
 	WHERE LOWER(name) = LOWER(language_name);
 
@@ -139,3 +162,13 @@ BEGIN
 	VALUES (title, release_year, language_id, 4.99, 3, 19.99);
 END;
 $$;
+
+
+/*BEGIN;
+
+CALL new_movie('The Great Adventure', 2024);
+
+ROLLBACK;
+*/
+
+COMMIT;
